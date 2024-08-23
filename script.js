a85edaff689fc78dd3551648143be52f4634d138
Josevdr95 <119988266+josevdr95new@users.noreply.github.com>
2024-08-23 11:36:59 -0400

    as

--- script.js
+++ script.js
@@ -124,3 +124,13 @@ document.addEventListener('DOMContentLoaded', () => {
     loadSavedSettings();
     fetchMessages(provinceSelect.value);
 });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registrado con éxito:', registration.scope);
+    })
+    .catch((error) => {
+      console.log('Error al registrar el Service Worker:', error);
+    });
+}
